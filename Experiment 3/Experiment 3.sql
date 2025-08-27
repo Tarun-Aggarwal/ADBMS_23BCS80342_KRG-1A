5bdfa3f920817ca2e94e783db99da04b3f402005
Tarun Aggarwal <tarunaggarwal0211@gmail.com>
2025-08-27 23:48:41 +0530

    Exp3 Medium Added

--- Experiment 3/Experiment 3.sql
+++ Experiment 3/Experiment 3.sql
@@ -11,17 +11,7 @@ create table employees_tbl(
 );
 
 insert into employees_tbl values
-(1),
-(1),
-(2),
-(3),
-(3),
-(4),
-(5),
-(5),
-(6),
-(7),
-(7);
+(1), (1), (2), (3), (3), (4), (5), (5), (6), (7), (7);
 
 select max(a.e_id) as max_distinct_id from (select e_id, count(e_id) as id_cnt from employees_tbl group by e_id) as a where a.id_cnt = 1;
 
@@ -60,3 +50,75 @@ select * from TBL_PRODUCTSALES
 select * from TBL_PRODUCTS where TBL_PRODUCTS.ID not in (select distinct PRODUCTID from TBL_PRODUCTSALES);
 
 select Name, (select SUM(TBL_PRODUCTSALES.QUALTITYSOLD) from TBL_PRODUCTSALES where PRODUCTID = TBL_PRODUCTS.ID) as [PRODUCT SALES] from TBL_PRODUCTS;
+
+
+--------------------EXPERIMENT 03: (MEDIUM LEVEL)
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
+
+-- Create Employee Table
+CREATE TABLE employee (
+    id INT,
+    name VARCHAR(50),
+    salary INT,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+
+-- Insert into Department Table
+INSERT INTO department (id, dept_name) VALUES
+(1, 'IT'),
+(2, 'SALES');
+
+-- Insert into Employee Table
+INSERT INTO employee (id, name, salary, department_id) VALUES
+(1, 'JOE', 70000, 1),
+(2, 'JIM', 90000, 1),
+(3, 'HENRY', 80000, 2),
+(4, 'SAM', 60000, 2),
+(5, 'MAX', 90000, 1);
+
+SELECT e.salary, e.name, t.m_salary, t.dept_name  
+FROM employee e
+JOIN (
+	SELECT e.department_id, d.dept_name, max(e.salary) AS m_salary 
+	FROM employee e 
+	JOIN department d
+    ON e.department_id = d.id  
+	GROUP BY e.department_id, d.dept_name
+) AS t 
+ON t.department_id = e.department_id 
+	AND t.m_salary = e.salary;
+
+
+-- Hard level
+create table emp_a_tbl(
+    empid int,
+    empname varchar(255),
+    salary int
+);
+
+create table emp_b_tbl(
+    empid int,
+    empname varchar(255),
+    salary int
+);
+
+insert into emp_a_tbl values
+(1, 'AA', 1000),
+(2, 'BB', 300);
+
+insert into emp_b_tbl values
+(2, 'BB', 400),
+(3, 'CC', 100);
+
+SELECT t.empid, min(t.empname), min(t.salary) 
+FROM (
+	SELECT * 
+	FROM emp_a_tbl 
+	UNION (SELECT * FROM emp_b_tbl)
+) t 
+GROUP BY t.empid;
